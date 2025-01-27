1cfd20c7aa445849818c4771e16b2a333821d9d8
Arturs Konevnikovs <artik292@gmail.com>
2025-01-27 16:02:23 +0200

    Debug

--- index.php
+++ index.php
@@ -1,37 +1,37 @@
 <?php
 
-// require "vendor/autoload.php";
+require "vendor/autoload.php";
 echo $_SERVER["SERVER_ADDR"];
 
-// $app = new App("public");
+$app = new App("public");
 
-// $button1 = $app->add(["Button", "Vecākiem", "massive red"]);
-// $button1->link(["parents"]);
-// $button2 = $app->add(["Button", "Skolotājiem", "massive green right floated"]);
-// $button2->link(["teachers_access"]);
+$button1 = $app->add(["Button", "Vecākiem", "massive red"]);
+$button1->link(["parents"]);
+$button2 = $app->add(["Button", "Skolotājiem", "massive green right floated"]);
+$button2->link(["teachers_access"]);
 
-// $reminder = $app->add(["ui" => "horizontal divider header"]);
-// // $reminder->set('Cienījamie vecāki! Lūdzam ievērot reglamentu: no 17.00 līdz 19.00.');
-// $text = new Model\Text($app->db);
-// $text->tryLoadBy("code", "main_page_middle_text");
-// $reminder->set($text["text"]);
+$reminder = $app->add(["ui" => "horizontal divider header"]);
+// $reminder->set('Cienījamie vecāki! Lūdzam ievērot reglamentu: no 17.00 līdz 19.00.');
+$text = new Model\Text($app->db);
+$text->tryLoadBy("code", "main_page_middle_text");
+$reminder->set($text["text"]);
 
-// $app->add(["ui" => "hidden divider"]);
+$app->add(["ui" => "hidden divider"]);
 
-// // $app->add(['Label','This app is made by Colibri School students','red right ribbon'])
-// // ->link('http://colibrischool.lv');
+// $app->add(['Label','This app is made by Colibri School students','red right ribbon'])
+// ->link('http://colibrischool.lv');
 
-// // $app->add(['Header','2018.gada 22.martā plkst. 17.00 - 19.00.','tiny','aligned' => 'center']);
-// $text = new Model\Text($app->db);
-// $text->tryLoadBy("code", "main_page_sub_text");
-// $app->add(["Header", $text["text"], "tiny", "aligned" => "center"]);
+// $app->add(['Header','2018.gada 22.martā plkst. 17.00 - 19.00.','tiny','aligned' => 'center']);
+$text = new Model\Text($app->db);
+$text->tryLoadBy("code", "main_page_sub_text");
+$app->add(["Header", $text["text"], "tiny", "aligned" => "center"]);
 
-// $counter = new Model\Counter($app->db);
-// $counter->tryLoadBy("id", "1");
-// if ($counter->loaded()) {
-//     $counter["counter"] = $counter["counter"] + 1;
-//     $counter->save();
-// } else {
-//     $counter["counter"] = 1;
-//     $counter->save();
-// }
+$counter = new Model\Counter($app->db);
+$counter->tryLoadBy("id", "1");
+if ($counter->loaded()) {
+    $counter["counter"] = $counter["counter"] + 1;
+    $counter->save();
+} else {
+    $counter["counter"] = 1;
+    $counter->save();
+}
